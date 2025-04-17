78b3c7b2c0eda98d2bb2529c5e2ae06ec5d78981
Loxizide <sofiane.bessila2@gmail.com>
2025-04-17 15:53:13 +0200

    Retravail db et ébauche algo

--- algo/main.py
+++ algo/main.py
@@ -1,14 +1,21 @@
 # main.py
 
 from user import Utilisateur
-from besoin import calcul_bmr, calcul_tdee
+from besoin import calcul_bmr, calcul_tdee, ajuster_objectif, calcul_macros
+
 
 # Exemple utilisateur
-u = Utilisateur(sexe="homme", age=25, poids=70, taille=175, activite=1.55, objectif="maintien")
+u = Utilisateur(sexe="homme", age=23, poids=71, taille=182, activite=1.725, objectif="maintien")
 
+
+# Calcul des besoins
 bmr = calcul_bmr(u)
 tdee = calcul_tdee(u)
+calories_obj = ajuster_objectif(tdee, u.objectif)
+proteines, glucides, lipides = calcul_macros(calories_obj, u.poids)
 
-print(f"TDEE : {tdee:.0f} kcal")
-
-
+print(f"TDEE ajusté ({u.objectif}) : {calories_obj:.0f} kcal")
+print(f"Répartition journalière :")
+print(f" - Protéines : {proteines:.0f} g")
+print(f" - Glucides  : {glucides:.0f} g")
+print(f" - Lipides   : {lipides:.0f} g")
